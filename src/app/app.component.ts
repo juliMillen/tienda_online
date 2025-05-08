74239984d457720256fa7f558a5c42b562b5e1e6
Julian Millen <julimillen@gmail.com>
2025-05-08 19:28:52 -0300

    mejorando la solucion y limpiando campos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoComponentsComponent } from "./components/listado-components/listado-components.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- src/app/components/listado-components/listado-components.component.ts
+++ src/app/components/listado-components/listado-components.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Producto } from '../../model/Producto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listado-components',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './listado-components.component.html',
   styleUrl: './listado-components.component.css'
 })
@@ -16,8 +17,22 @@ export class ListadoComponentsComponent {
     new Producto('Remera S',60)
   ];
 
-  agregarProducto(descProd:string, precioProd:number) {
-      var productoNuevo = new Producto(descProd,precioProd);
-      this.listaProductos.push(productoNuevo);
+
+  descripcionInput: string = '';
+  precioInput: number | null =null;
+
+  agregarProducto() {
+      // validar que sean valores correctos
+      if(this.descripcionInput.trim() === '' || this.precioInput == null || this.precioInput == 0){
+        console.log('Debe ingresar una descripcion y un precio validos');
+        return;
+      }
+
+      const producto = new Producto(this.descripcionInput,this.precioInput);
+      this.listaProductos.push(producto);
+
+      //limpiar campos
+      this.descripcionInput = '';
+      this.precioInput = null;
     }
 }
