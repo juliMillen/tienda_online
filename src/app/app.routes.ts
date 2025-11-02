d3b568e91197292bccfa8b75208fcf7647c8ac14
Julian Millen <julimillen@gmail.com>
2025-11-02 18:12:48 -0300

    Comenzando con Login en Firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ListadoComponentsComponent } from './components/listado-components/listado-components.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path:'', component: ListadoComponentsComponent},
     {path: 'listado', component: ListadoComponentsComponent},
     {path: 'agregar', component: FormularioComponent},
     {path: 'editar/:llave', component: FormularioComponent},
+    {path: 'login', component:LoginComponent},
     {path: '**', component: ErrorComponent}
 ];
